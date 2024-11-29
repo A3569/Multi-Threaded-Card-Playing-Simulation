6d5cfb96a6b5758b277194eb1c30d7b88cccc3c7
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-11-29 15:15:20 +0000

    Update CardGame.java

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,9 +1,15 @@
 public class CardGame {
   // Main method to run the game
   public static void main (String[] args){
+    System.out.println("Welcome to the Card Game!");
+    System.out.println("Initializing the game...");
     
+    try{
+      
+    } catch (Exception e){
+            e.printStackTrace();
+        }
   }
-  
   // Starts and runs the game
   // Sets up the card game
   // Ends to the game
