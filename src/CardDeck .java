e644a9c0aae2103fc6a7c1f56dd891768212a6b6
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-11-29 14:56:27 +0000

    Update CardDeck .java

--- src/CardDeck .java
+++ src/CardDeck .java
@@ -1,3 +1,10 @@
 public class CardDeck {
-
+  // Private Variables
+  // Get methods
+  // Constructor
+  // Sets the initial cards of the deck
+  // Adds a card to the deck
+  // Removes the top card from the deck
+  // Creates a new output file for the deck
+  // Writes the deck's final values to the file and closes it
 }
