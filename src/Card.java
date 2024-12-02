1f722769079d290744e9ca0282ac9d205c206b34
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-12-02 02:03:13 +0000

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,15 @@
-public class Card {
-  // Represents an individual card in the game with a rank
-  
+public class Card { 
+  // Represents an individual card in the game
+  // Private attribute
+  private final int number;
+
+  // Created a constructor
+  public Card(int number) {
+    this.number = number;
+  }
+
+  // Get method for the card
+  public int getNumber() {
+    return number;
+  }
 }
