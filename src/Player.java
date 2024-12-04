0ba01c16ad2bc2f7c44e9d85685c5b072b601060
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-12-04 14:32:13 +0000

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -4,7 +4,17 @@ public class Player {
   // Checks that all cards are the same
   // get rid of unwanted card 
   // Player's final hand
-  // Creates a new output file for the player's log
+  
   // Updates the player's file
+  public writeOutputfile() {
+    try{
+      FileWriter fw = new FileWriter("./output/player" + id + "_output.txt");
+    fw.write("player" + id + "contents: " + this.());
+    fw.close();
+  } catch (Exception e){
+            e.printStackTrace();
+        }
+  }
+  
   // Writes to the file to record the final cards and closes the file
 }
