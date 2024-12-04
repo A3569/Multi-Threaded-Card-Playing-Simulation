f71ceab64a764df5e4d65a78a39a68f2ba352986
Ev <evelyn@thejohnnos.com>
2024-12-04 14:33:10 +0000

    Update CardGame.java

--- src/CardGame.java
+++ src/CardGame.java
@@ -15,12 +15,16 @@ import java.util.Scanner;
 
 class Main {                                             // when trying to run on VSC, keeps coming up that Main could not be loaded?
   public static void main(String[]args) {
-      Scanner players = new Scanner(System.in); 
+      Scanner beginGame = new Scanner(System.in); 
       System.out.printIn("Please enter the number of players in the group:");
 
-      int playersNumber = players.nextInt();    // get user input of players
-      
-      System.out.printIn = ("Number of players: " + player);  // print results 
+      int playersNumber = beginGame.nextInt();                           // get user input of players
+      System.out.printIn = ("Number of players: " + playersNumber);      // print results 
+
+      System.out.printIn("Please enter location of card deck to load:");
+
+      String deckLocation = beginGame.nextInt();                         // get location of deck of cards
+      System.out.printIn = ("Location of deck: " + deckLocation);        // print results 
 
   }
 }
