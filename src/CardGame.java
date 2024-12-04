b69f7b5099e75bc1a4cd8a9d9201633be448ced1
Ev <evelyn@thejohnnos.com>
2024-12-04 14:26:42 +0000

    Update CardGame.java

--- src/CardGame.java
+++ src/CardGame.java
@@ -11,6 +11,21 @@ public class CardGame {
         }
   }
   // Starts and runs the game
+import java.util.Scanner;
+
+class Main {                                             // when trying to run on VSC, keeps coming up that Main could not be loaded?
+  public static void main(String[]args) {
+      Scanner players = new Scanner(System.in); 
+      System.out.printIn("Please enter the number of players in the group:");
+
+      int playersNumber = players.nextInt();    // get user input of players
+      
+      System.out.printIn = ("Number of players: " + player);  // print results 
+
+  }
+}
+  
+
   // Sets up the card game
   // Ends to the game
 }
