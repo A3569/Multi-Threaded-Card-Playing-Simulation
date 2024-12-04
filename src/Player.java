19663eff0565eb4f672bd8ef0859d69309eb4b92
Ev <evelyn@thejohnnos.com>
2024-12-04 13:23:27 +0000

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,7 @@ public class Player {
   // Represents a player in the game, manages their hand, and handles game logic specific to players
   // Player's hand
   // Checks that all cards are the same
+  // get rid of unwanted card 
   // Player's final hand
   // Creates a new output file for the player's log
   // Updates the player's file
