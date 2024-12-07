05425361ab8d940edbfd0c2165e29e3a80ba24fe
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-12-07 02:42:48 +0000

    Update testCard.java

--- test/testCard.java
+++ test/testCard.java
@@ -1,1 +1,28 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+public class testCard {
+    @Test
+    public void testInitialization() {
+        // Create a card with a specific number
+        int cardNumber = 5;
+        Card card = new Card(cardNumber);
+        assertEquals("Card number should match the initialized value", cardNumber, card.getNumber());
+    }
+
+    @Test
+    public void testCardWithNegativeNumber() {
+        // Create a card with a negative number
+        int cardNumber = -3;
+        Card card = new Card(cardNumber);
+        assertEquals("Card number should match the initialized value", cardNumber, card.getNumber());
+    }
+
+    @Test
+    public void testCardWithZero() {
+        // Create a card with a number of zero
+        int cardNumber = 0;
+        Card card = new Card(cardNumber);
+        assertEquals("Card number should match the initialized value", cardNumber, card.getNumber());
+    }
+}
