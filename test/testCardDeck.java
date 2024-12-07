700a9a2d163c3de15e78de43808a740977acafc3
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-12-07 22:45:01 +0000

    Update testCardDeck.java

--- test/testCardDeck.java
+++ test/testCardDeck.java
@@ -1,1 +1,53 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.File;
 
+public class testCardDeck {
+    private CardDeck carddeck;
+
+    @Before
+    public void setUp() {
+    	carddeck = new CardDeck(1); // Create a deck with ID 1
+    }
+
+    @Test
+    public void testAddCard() {
+        Card card = new Card(5);
+        carddeck.addCard(card);
+        assertEquals("Deck 1 contents: 5", carddeck.getFinalDeck());
+    }
+
+    @Test
+    public void testRemoveCard() {
+        Card card1 = new Card(3);
+        Card card2 = new Card(7);
+        carddeck.addCard(card1);
+        carddeck.addCard(card2);
+        assertEquals(3, carddeck.removeCard().getNumber());
+        assertEquals("Deck 1 contents: 7", carddeck.getFinalDeck());
+    }
+
+    @Test
+    public void testGetFinalDeck() {
+        Card card1 = new Card(1);
+        Card card2 = new Card(2);
+        Card card3 = new Card(3);
+        carddeck.addCard(card1);
+        carddeck.addCard(card2);
+        carddeck.addCard(card3);
+        assertEquals("Deck 1 contents: 1 2 3", carddeck.getFinalDeck());
+    }
+    
+    @Test
+    public void testWriteOutputFile() {
+        Card card1 = new Card(3);
+        Card card2 = new Card(7);
+        carddeck.addCard(card1);
+        carddeck.addCard(card2);
+        carddeck.writeOutputFile();
+        File outputFile = new File("./output/deck1_output.txt");
+        assertTrue(outputFile.exists());
+        assertTrue(outputFile.length() > 0);
+    }
+}
