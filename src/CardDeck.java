96a084d2a21159afd09dba84505095aff96f684b
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-12-08 18:45:23 +0000

    Update CardDeck.java

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -4,27 +4,27 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class CardDeck {
-  // Private attribute
+  // Attributes
   private final ArrayList<Card> cards;
   private final int id;
   
-  // Constructor
+  // Constructors
   public CardDeck(int id) {
     this.id = id;
     this.cards = new ArrayList<>();
   }
 
-  // Get ID
+  // gets deck's ID
   public int getID() {
     return id;
   }
 
-  // Get cards
+  // gets the player's cards
   public ArrayList<Card> getCards() {
     return cards;
   }
   
-  // Adds a card to the deck
+  // adds a card to the deck
   public synchronized void addCard(Card card) {
     if (card == null) {
       throw new IllegalArgumentException("Card cannot be null.");
@@ -32,7 +32,7 @@ public class CardDeck {
     cards.add(card);
   }
   
-  // Removes the top card from the deck
+  // removes the top card from the deck
   public synchronized Card removeCard() {
       if (cards.isEmpty()) {
           throw new IllegalStateException("The deck " + id + " is empty.");
@@ -40,29 +40,21 @@ public class CardDeck {
       return cards.remove(0);
   }
 
-  // Writes the deck's final values to the file
+  // writes the deck's final values to the file
   public synchronized void writeOutputFile() {
     try{
-    	File outputDir = new File("./output");
-        if (!outputDir.exists()) {
-            outputDir.mkdirs();
-        }
-        
-        File newFile = new File("./output/deck"+id+"_output.txt");
-    	if(newFile.createNewFile()){
-            System.out.println("File is created");
-        } else {
-            System.out.println("File is already exists");
-        }
-    	
-    	try (FileWriter fw = new FileWriter(newFile)) {
-            fw.write(getFinalDeck());
-    	}
-    	} catch (IOException e) {
-            System.err.println("Error writing for deck " + id);
-            e.printStackTrace();
-            }
+      if (fw == null) {
+        fw = new FileWriter("./output/deck" + id + "_output.txt", false);
+        fw = write(" ");
+        fw.close();
+        fw = new FileWriter("./output/deck" + id + "_output.txt", true);
+        fw.write(getFinalDeck());
+      }
+    } catch (IOException e) {
+      System.err.println("Error writing for deck " + id);
+      e.printStackTrace();
     }
+  }
 
   // contain the contents of the deck at the end of the game
   public synchronized String getFinalDeck() {
