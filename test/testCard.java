927aedb6beefd6c2966d9ecdfc749e4faef9f1cb
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-12-09 01:03:30 +0000

    Update testCard.java

--- test/testCard.java
+++ test/testCard.java
@@ -2,36 +2,32 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class testCard {
-    @Test
-    public void testInitialization() {
-        // Create a card with a specific number
-        int cardNumber = 5;
-        Card card = new Card(cardNumber);
-        assertEquals(cardNumber, card.getNumber());
-    }
-
-    @Test
-    public void testCardWithNegativeNumber() {
-        // Create a card with a negative number
-        int cardNumber = -3;
-        Card card = new Card(cardNumber);
-        assertEquals(cardNumber, card.getNumber());
+	@Test
+    public void testCardInitialization() {
+        // Test that the card initializes correctly
+        Card card = new Card(5);
+        assertEquals(5, card.getNumber());
     }
 
     @Test
     public void testCardWithZero() {
-        // Create a card with a number of zero
-        int cardNumber = 0;
-        Card card = new Card(cardNumber);
-        assertEquals(cardNumber, card.getNumber());
+        // Test initializing card with zero
+        Card card = new Card(0);
+        assertEquals(0, card.getNumber());
     }
 
     @Test
-    public void testMistmatchCardNumber(){
-        // Test mismatched numbers
-        int cardNumber = 4;
-        int mismatchedNumber = 3;
-        Card card = new Card(cardNumber);
-        assertFalse(card.getNumber() == mismatchedNumber);
+    public void testCardWithNegativeNumber() {
+        // Test initializing card with a negative number
+        Card card = new Card(-1);
+        assertEquals(-1, card.getNumber());
+    }
+
+    @Test
+    public void testCardWithLargeNumber() {
+        // Test initializing card with a large number
+        int largeNumber = Integer.MAX_VALUE;
+        Card card = new Card(largeNumber);
+        assertEquals(largeNumber, card.getNumber());
     }
 }
