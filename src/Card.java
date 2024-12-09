a3f0b413d682e49085bbabf813f0c1e012ce5875
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-12-09 03:15:42 +0000

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,14 +1,14 @@
-public class Card { 
-  // Attribute
-  private final int number;
+public class Card {
+    // Attribute
+    private final int number;
 
-  // Constructor
-  public Card(int number) {
-    this.number = number;
-  }
+    // Constructor
+    public Card(int number) {
+        this.number = number;
+    }
 
-  // gets the card's number
-  public int getNumber() {
-    return number;
-  }
+    // gets the card's number
+    public int getNumber() {
+        return number;
+    }
 }
